c1028e94c1156a48822b4d5d7f198c80d1f91235
seymakarahan <85848981+seymakarahan@users.noreply.github.com>
2022-06-14 15:40:08 +0300

    Create odev-1.1.sql

---
+++ odev-1.1.sql
@@ -0,0 +1,5 @@
+select title,description from film; --(1. Madde)
+select * from film where length>60 and length<75; --(2. Madde)
+select * from film where rental_rate=0.99 and  replacement_cost=12.99 or  replacement_cost=28.99 --(3. Madde)
+select * from customer where first_name = 'Mary' --(4. Madde)
+select * from film where not (length > 50 and (rental_rate = 2.99 or rental_rate = 4.99)) --(5. Madde)
